f877cb0c8f9d3e266f26e1cd2cf88bc280933684
highjeans <highjeans11@hotmail.com>
2022-12-31 17:53:11 +0000

    Create body parser

--- handlers.js
+++ handlers.js
@@ -1,3 +1,19 @@
+function parseBody(request) {
+    let chunks = [];
+    let body = {};
+    request.on("data", (chunk) => {
+        chunks.push(chunk);
+    });
+    request.on("end", () => {
+        chunks = Buffer.concat(chunks).toString();
+        for (const [key, value] of new URLSearchParams(chunks)) {
+            body[key] = value;
+        }
+    });
+    console.log(body);
+    return body;
+}
+
 const getRecipes = (request, response) => {
 
 };
