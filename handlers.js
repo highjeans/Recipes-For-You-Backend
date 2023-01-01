676f36a7c69e9deece09f42de47f0c4fc07be2d6
highjeans <highjeans11@hotmail.com>
2023-01-01 12:43:46 +0000

    Update Card REST API

--- handlers.js
+++ handlers.js
@@ -1,10 +1,8 @@
 const apikeyString = "?apiKey=1aadbb8303e74d88ba0e691bc5b59c11";
 
-function parseData(chunks) {
-    let body = {};
-    const chunksArr = Buffer.concat(chunks);
-    const chunksStr = chunksArr.toString();
-    const urlParams = new URLSearchParams(chunksStr);
+function parseData(bodyStr) {
+    const urlParams = new URLSearchParams(bodyStr);
+    let body = {}
     for (const [key, value] of urlParams.entries()) {
         body[key] = value;
     }
@@ -36,22 +34,14 @@ const getRecipe = async (request, response) => {
 };
 
 const getCard = async (request, response) => {
-    let chunks = [];
-    request.on("data", (chunk) => {
-        chunks.push(chunk);
-    });
-    request.on("end", async () => {
-        const body = parseData(chunks);
-        const id = body["id"];
+    const id = parseData(getBody(request))["id"];
+    const card = await fetch(`https://api.spoonacular.com/recipes/${id}/card` + apikeyString);
+    const json = await card.json();
+    const url = json["url"];
 
-        const card = await fetch(`https://api.spoonacular.com/recipes/${id}/card` + apikeyString);
-        const json = await card.text();
-        const url = JSON.parse(json)["url"];
-
-        response.writeHead(200, {"Content-Type": "application/json"});
-        response.write(JSON.stringify({url: url}));
-        response.end();
-    });
+    response.writeHead(200, {"Content-Type": "application/json"});
+    response.write(JSON.stringify({url: url}));
+    response.end();
 };
 
 module.exports = {getCard, getRecipe, getRecipes};
