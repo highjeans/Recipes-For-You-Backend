3df2f006a3b6e08ef03053222516a109e21600ce
highjeans <highjeans11@hotmail.com>
2023-01-01 01:27:53 +0000

    Update body parser

--- handlers.js
+++ handlers.js
@@ -1,16 +1,12 @@
 function parseBody(request) {
     let chunks = [];
     let body = {};
-    request.on("data", (chunk) => {
-        chunks.push(chunk);
-    });
-    request.on("end", () => {
-        chunks = Buffer.concat(chunks).toString();
-        for (const [key, value] of new URLSearchParams(chunks)) {
-            body[key] = value;
-        }
-    });
-    console.log(body);
+    const chunksArr = Buffer.concat(chunks);
+    const chunksStr = chunksArr.toString();
+    const urlParams = new URLSearchParams(chunksStr);
+    for (const [key, value] of urlParams.entries()) {
+        body[key] = value;
+    }
     return body;
 }
 
